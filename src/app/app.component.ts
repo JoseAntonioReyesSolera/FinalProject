2b40f55ebb6cab3df9382e0ba695f1b9d0f097e6
Jose Antonio Reyes <antonio.reyes@mindata.es>
2025-04-01 15:05:01 +0200

    Sercher api cards

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import {Component, NgModule} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ScryfallService } from './services/scryfall.service';
+import { BattlefieldComponent } from './components/battlefield/battlefield.component';
+import { HandComponent } from './components/hand/hand.component';
+import { StackComponent } from './components/stack/stack.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    FormsModule,
+    HandComponent,
+    BattlefieldComponent,
+    StackComponent
+  ],
+  template: `
+    <app-battlefield></app-battlefield>
+    <app-hand></app-hand>
+    <app-stack></app-stack>
+  `,
+  providers: [ScryfallService]
+})
+export class AppComponent { }
