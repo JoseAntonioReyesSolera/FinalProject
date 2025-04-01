27b293fb4bf886cb7ac0fab56ce1aece4ada3ced
Jose Antonio Reyes <antonio.reyes@mindata.es>
2025-04-01 15:39:22 +0200

    View info card

--- src/app/components/hand/hand.component.ts
+++ src/app/components/hand/hand.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ScryfallService } from '../../services/scryfall.service';
 import {FormsModule} from '@angular/forms';
+import {Cart} from '../../models/cart';
 
 @Component({
   selector: 'app-hand',
@@ -12,18 +13,31 @@ import {FormsModule} from '@angular/forms';
 })
 export class HandComponent {
   cardName: string = '';
-  cardImage: string | undefined;
+  card: Cart | undefined;
 
   constructor(private scryfallService: ScryfallService) {}
 
   searchCard() {
     this.scryfallService.getCardByName(this.cardName).subscribe({
       next: (card) => {
-        this.cardImage = card.image_uris?.normal;
+        // Asignamos todos los valores al modelo Cart
+        this.card = {
+          id: card.id,
+          name: card.name,
+          image_uris: card.image_uris, // Se pueden acceder a normal, art_crop, etc.
+          oracle_text: card.oracle_text,
+          type_line: card.type_line,
+          mana_cost: card.mana_cost,
+          cmc: card.cmc,
+          power: card.power,
+          toughness: card.toughness,
+          keywords: card.keywords,
+          produced_mana: card.produced_mana
+        };
       },
       error: (err) => {
         console.error('Error fetching card', err);
-        this.cardImage = undefined;
+        this.card = undefined; // Si ocurre un error, no se muestra la carta.
       },
     });
   }
